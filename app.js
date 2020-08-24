1dc4d0b2c86d3130ed1f4d38bb1724815d45894c
Ayan Pal <contact@ayanpal.co.in>
2020-08-25 02:06:28 +0530

    intermediate commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const httpClient = require('postman-request');
 
-const url = 'http://api.weatherstack.com/current?access_key=4452811edc6bdb2f04ac6734efba18bb&query=22.566898,88.273911';
+const url = 'http://api.weatherstack.com/current?access_key=4452811edc6bdb2f04ac6734efba18bb&query=22.566898,88.273911&units=f';
 
-const data = httpClient.get(url, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+const data = httpClient.get(url, {json:true}, (error, response) => {
+    const currentWeatherData = response.body.current;
+    console.log(currentWeatherData.weather_descriptions[0] + '. Its is currently '+currentWeatherData.temperature+' degrees out. But feels like '+currentWeatherData.feelslike+' degrees.');
 });
